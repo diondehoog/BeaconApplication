789a8f90f07f7bae6f924091ea55fc6cd46d9773
Rolf Hut <r.w.hut@tudelft.nl>
2017-10-16 13:08:07 +0200

    initial commit of pico firmware
    firmware that advertises for 1 second, is silent for 5 seconds.

---
+++ pycomFirmware/main.py
@@ -0,0 +1,24 @@
+import pycom
+from network import Bluetooth
+import time
+
+bluetooth = Bluetooth()
+
+def conn_cb (bt_o):
+    events = bt_o.events()   # this method returns the flags and clears the internal registry
+    if events & Bluetooth.CLIENT_CONNECTED:
+        print("Client connected")
+    elif events & Bluetooth.CLIENT_DISCONNECTED:
+        print("Client disconnected")
+
+bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=conn_cb)
+
+counter = 0
+
+while True:
+    bluetooth.set_advertisement(name='LoPyRolf', service_uuid='1234567890123456', service_data=str(counter))
+    bluetooth.advertise(True)
+    time.sleep(5)
+    bluetooth.advertise(False)
+    counter = counter + 1
+    time.sleep(1)
